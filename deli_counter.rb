b76a4e6e7f594505b2b6ff3655985b698f2be758
Choeles Tung <choelestung@gmail.com>
2018-06-09 02:55:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -40,7 +40,7 @@ end
 
 def take_a_number(katz_deli, name) 
   katz_deli << name
-  puts "Welcome, #{katz_deli.length}. #{name}"
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line. "
   
 end
 # binding.pry
