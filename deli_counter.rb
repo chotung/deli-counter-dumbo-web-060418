8226b57f1f2ab67434175e39f0989e85bf92daeb
Choeles Tung <choelestung@gmail.com>
2018-06-09 02:53:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,46 @@
-# Write your code here.
+require 'pry'
+# # Write your code here.
+
+# def array = [];         // where is your line?
+  
+# def line(spot) {        // function
+#   def i = 0;            
+#   return spot[i+1];     // give them their number 
+#                         // 
+# }
+# end
+
+
+# function line(name) {
+#   console.log(name);
+# }
+
+
+
+# def line = [];
+# function thisIsTheLine(name) {
+#   for (i = 0, i < thisIsTheLine.length, i++) {
+#     thisIsTheLine.push(name);
+#   }
+#   return line;
+# }
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length > 0
+    katz_deli
+    # binding.pry
+  else  
+    puts "The line is currently empty."
+   end 
+end
+# binding.pry
+
+
+def take_a_number(katz_deli, name) 
+  katz_deli << name
+  puts `#{katz_deli.length}. #{name}`
+  
+end
+# binding.pry
